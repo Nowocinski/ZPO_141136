ef0b94c58a2a99a4883e783f3206d67a30f320e1
Rafał Nowociński <vito24222@gmail.com>
2020-03-08 19:50:49 +0100

    Dodanie zadania A17 i A18

--- Ćwiczenia 1/A17/A17/Program.cs
+++ Ćwiczenia 1/A17/A17/Program.cs
@@ -4,8 +4,21 @@ namespace A17
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            Console.Write("Podaj liczbę w systemie szesnastkowym: ");
+            var tekst = Console.ReadLine();
+
+            try
+            {
+                Console.WriteLine($"Liczba {tekst} w systemie dziesiętnym to {Convert.ToInt32(tekst, 16)}");
+            }
+            catch
+            {
+                Console.WriteLine($"Nie można zamienić liczby {tekst} na system dziesiętny");
+            }
+
+            Console.ReadKey();
         }
     }
 }
---
+++ Ćwiczenia 1/A18/A18/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace A18
+{
+    class Program
+    {
+        static void Main()
+        {
+            var vektor1 = new List<int>();
+            var vektor2 = new List<int>();
+            int liczba;
+
+            Console.WriteLine("Podaj wartości pierwszego wektora używając entera (aby zakończyć podaj wartość, która nie jest liczbą):");
+            while (int.TryParse(Console.ReadLine(), out liczba))
+            {
+                vektor1.Add(liczba);
+            }
+
+            Console.WriteLine("Podaj wartości drugiego wektora używając entera (aby zakończyć podaj wartość, która nie jest liczbą):");
+            while (int.TryParse(Console.ReadLine(), out liczba))
+            {
+                vektor2.Add(liczba);
+            }
+
+            if (vektor1.Count != vektor2.Count)
+            {
+                Console.WriteLine("Wektory mają różną długość. Nie można policzyć iloczynu skalarnego.");
+            }
+
+            Console.WriteLine($"Iloczyn skalarny: {vektor1.Select((t, i) => t * vektor2[i]).Sum()}");
+            Console.ReadKey();
+        }
+    }
+}
