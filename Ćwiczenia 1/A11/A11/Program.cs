ae758e6d7ed8df9c1e7a8175689835aeef011d9a
Rafał Nowociński <vito24222@gmail.com>
2020-03-08 18:03:20 +0100

    Dodanie zadania A11

---
+++ Ćwiczenia 1/A11/A11/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace A11
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Wprowadź tekst: ");
+            var tekst = Console.ReadLine();
+
+            Console.WriteLine(tekst.ToUpper());
+
+            Console.ReadKey();
+        }
+    }
+}
