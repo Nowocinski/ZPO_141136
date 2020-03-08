0a67d6aea3ea018813dfb51fef5ba6c271942bd4
Rafał Nowociński <vito24222@gmail.com>
2020-03-08 17:09:50 +0100

    Dodanie zadania A5

---
+++ Ćwiczenia 1/A5/A5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace A5
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Podaj pierwszą liczbę: ");
+            int liczba1;
+            while (!int.TryParse(Console.ReadLine(), out liczba1))
+            {
+                Console.Write("Podana wartość nie jest liczbą całkowitą. Podaj liczbę: ");
+            }
+
+            Console.Write("Podaj drugą liczbę: ");
+            int liczba2;
+            while (!int.TryParse(Console.ReadLine(), out liczba2))
+            {
+                Console.Write("Podana wartość nie jest liczbą całkowitą. Podaj liczbę: ");
+            }
+
+            Console.Write("Podaj trzecią liczbę: ");
+            int liczba3;
+            while (!int.TryParse(Console.ReadLine(), out liczba3))
+            {
+                Console.Write("Podana wartość nie jest liczbą całkowitą. Podaj liczbę: ");
+            }
+
+            Console.WriteLine($"Wynik mnożenia liczb {liczba1}, {liczba2}, {liczba3} to {liczba1 * liczba2 * liczba3}");
+            Console.ReadKey();
+        }
+    }
+}
