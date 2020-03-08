669236e347350ba1922fa7cf7247e9de4f3a0edc
Rafał Nowociński <vito24222@gmail.com>
2020-03-08 17:46:09 +0100

    Dodanie zadania A8

---
+++ Ćwiczenia 1/A8/A8/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace A8
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Podaj temperaturę w stopniach °C: ");
+            double temperaturaC;
+            while (!double.TryParse(Console.ReadLine(), out temperaturaC))
+            {
+                Console.Write("Podana wartość nie jest liczbą. Podaj liczbę: ");
+            }
+
+            Console.WriteLine($"°K: {temperaturaC + 273.15}");
+            Console.WriteLine($"°F: {32 + 9.0/5.0 * temperaturaC}");
+            Console.ReadKey();
+        }
+    }
+}
