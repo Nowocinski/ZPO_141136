4ecd1ecc719c066edade44bf7f4876681069a9a8
Rafał Nowociński <vito24222@gmail.com>
2020-03-08 20:31:37 +0100

    Dodanie zadania A23

---
+++ Ćwiczenia 1/A23/A23/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace A23
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Białystok (297 459)");
+            Console.WriteLine("Bydgoszcz (350 178)");
+            Console.WriteLine("Gdańsk (466 631)");
+            Console.WriteLine("Gorzów Wielkopolski (124 295)");
+            Console.WriteLine("Katowice (294 510)");
+            Console.WriteLine("Kielce (195 774)");
+            Console.WriteLine("Lublin (339 682)");
+            Console.WriteLine("Łódź (682 679)");
+            Console.WriteLine("Olsztyn (173 125)");
+            Console.WriteLine("Opole (128 208)");
+            Console.WriteLine("Poznań (535 802)");
+            Console.WriteLine("Rzeszów (195 734)");
+            Console.WriteLine("Szczecin (402,1 tys.)");
+            Console.WriteLine("Toruń (201 798)");
+            Console.WriteLine("Warszawa (1 777 972)");
+            Console.WriteLine("Wrocław (641 607)");
+            Console.WriteLine("Zielona Góra (140 874)");
+            Console.ReadKey();
+        }
+    }
+}
