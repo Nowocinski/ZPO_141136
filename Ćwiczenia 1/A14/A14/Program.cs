851f23ee5e86e3565e979c0ab4bbaaa6e5ca1b1e
Rafał Nowociński <vito24222@gmail.com>
2020-03-08 18:47:23 +0100

    Dodanie zadania A14

---
+++ Ćwiczenia 1/A14/A14/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace A14
+{
+    class Program
+    {
+        static void Main()
+        {
+            int suma = 0;
+            int licznik = 0;
+            int liczba = 1;
+
+            while (licznik <= 100)
+            {
+                bool istniejeDzielnik = false;
+                for (var i = 1; i < liczba; i++)
+                {
+                    if (i != liczba && i != 1 && liczba%i == 0)
+                    {
+                        istniejeDzielnik = true;
+                        break;
+                    }
+                }
+
+                if (istniejeDzielnik == false)
+                {
+                    suma += liczba;
+                    licznik++;
+                }
+
+                liczba++;
+            }
+
+            Console.WriteLine($"Suma 100 pierwszych liczb pierwszych: {suma}");
+            Console.ReadKey();
+        }
+    }
+}
