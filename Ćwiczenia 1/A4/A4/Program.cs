bf443cb08eed42d7de849e1c4e4d86e21baee47c
Rafał Nowociński <vito24222@gmail.com>
2020-03-08 17:04:11 +0100

    Dodanie zadania A3

---
+++ Ćwiczenia 1/A4/A4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace A4
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Podaj pierwszą liczbę: ");
+            double liczba1;
+            while (!double.TryParse(Console.ReadLine(), out liczba1))
+            {
+                Console.Write("Podana wartość nie jest liczbą. Podaj liczbę: ");
+            }
+
+            Console.Write("Podaj drugą liczbę: ");
+            double liczba2;
+            while (!double.TryParse(Console.ReadLine(), out liczba2))
+            {
+                Console.Write("Podana wartość nie jest liczbą. Podaj liczbę: ");
+            }
+
+            Console.WriteLine(liczba2);
+            Console.WriteLine(liczba1);
+            Console.ReadKey();
+        }
+    }
+}
