321881db96d3cb4ebe6c4639572ce87cbeef6ddb
Rafał Nowociński <vito24222@gmail.com>
2020-03-08 18:26:29 +0100

    Dodanie zadania A12

---
+++ Ćwiczenia 1/A12/A12/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace A12
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Wprowadź tekst: ");
+            var tekst = Console.ReadLine() + " ";
+
+            string najdłuższeSłowo = "";
+            var słowo = "";
+
+            for (var i = 0; i < tekst.Length; i++)
+            {
+                var znak = tekst[i];
+                if (znak != ' ')
+                {
+                    słowo += znak;
+                }
+                else
+                {
+                    if (słowo.Length > najdłuższeSłowo.Length)
+                    {
+                        najdłuższeSłowo = słowo;
+                        słowo = "";
+                    }
+                }
+            }
+
+            Console.WriteLine($"Najdłuższe słowo to: {najdłuższeSłowo}");
+            Console.ReadKey();
+        }
+    }
+}
