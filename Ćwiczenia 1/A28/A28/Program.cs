6812865b91bb6f719b7cbaa7f12fc2839146848b
Rafał Nowociński <vito24222@gmail.com>
2020-03-08 21:07:51 +0100

    Dodanie zadania A28

---
+++ Ćwiczenia 1/A28/A28/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace A28
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Podaj promień kuli: ");
+            double R;
+            while (!double.TryParse(Console.ReadLine(), out R) || R <= 0)
+            {
+                if (R <= 0)
+                {
+                    Console.Write("Liczba musi być większa od zera. Podaj poprawną liczbę: ");
+                    continue;
+                }
+
+                Console.Write("Podana wartość nie jest liczbą. Podaj liczbę: ");
+            }
+
+            Console.WriteLine($"Pole: {4 * Math.PI * R * R}");
+            Console.WriteLine($"Objętość: {4.0/3.0 * Math.PI * R * R * R}");
+            Console.ReadKey();
+        }
+    }
+}
