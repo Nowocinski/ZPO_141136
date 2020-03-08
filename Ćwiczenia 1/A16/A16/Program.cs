c94ed7bcc5f587f8b0ece78853ea5bc303848f5b
Rafał Nowociński <vito24222@gmail.com>
2020-03-08 19:07:05 +0100

    Dodanie zadania A16 i A17

---
+++ Ćwiczenia 1/A16/A16/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace A16
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Wprowadź tekst: ");
+            var tekst = Console.ReadLine() + " ";
+
+            var słowa = new List<string>();
+            var słowo = "";
+            foreach (var znak in tekst)
+            {
+                if (znak != ' ')
+                {
+                    słowo += znak;
+                }
+                else
+                {
+                    słowa.Add(słowo);
+                    słowo = "";
+                }
+            }
+
+            słowa.Reverse();
+
+            foreach (var słowoZListy in słowa)
+            {
+                Console.Write($"{słowoZListy} ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Ćwiczenia 1/A17/A17/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace A17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
