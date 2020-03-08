6ea364e03c17877db0488fe14d2e66d9bffa70c9
Rafał Nowociński <vito24222@gmail.com>
2020-03-08 17:38:49 +0100

    Dodanie zadania A7

---
+++ Ćwiczenia 1/A7/A7/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace A7
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Podaj pierwszą liczbę: ");
+            double liczba1;
+            while (!double.TryParse(Console.ReadLine(), out liczba1))
+            {
+                Console.Write("Podana wartość nie jest liczbą. Podaj liczbę: ");
+            }
+
+            Console.Write("Podaj drugą liczbę: ");
+            double liczba2;
+            while (!double.TryParse(Console.ReadLine(), out liczba2))
+            {
+                Console.Write("Podana wartość nie jest liczbą. Podaj liczbę: ");
+            }
+
+            Console.Write("Podaj trzecią liczbę: ");
+            double liczba3;
+            while (!double.TryParse(Console.ReadLine(), out liczba3))
+            {
+                Console.Write("Podana wartość nie jest liczbą. Podaj liczbę: ");
+            }
+
+            Console.WriteLine($"Średnia arytemtyczna: {(liczba1 + liczba2 + liczba3)/3}");
+            Console.WriteLine($"Średnia geometryczna: {Math.Pow(liczba1 * liczba2 * liczba3, 1.0/3.0)}");
+            Console.ReadKey();
+        }
+    }
+}
