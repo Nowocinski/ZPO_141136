dc7474df3035b786694a2d5419a51242ee00a488
Rafał Nowociński <vito24222@gmail.com>
2020-03-08 21:02:39 +0100

    Dodanie zadania A27

---
+++ Ćwiczenia 1/A27/A27/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace A27
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Podaj długość pierwszego boku: ");
+            double a;
+            while (!double.TryParse(Console.ReadLine(), out a) || a <= 0)
+            {
+                if (a <= 0)
+                {
+                    Console.Write("Liczba musi być większa od zera. Podaj poprawną liczbę: ");
+                    continue;
+                }
+
+                Console.Write("Podana wartość nie jest liczbą. Podaj liczbę: ");
+            }
+
+            Console.WriteLine($"Pole: {a * a}");
+            Console.WriteLine($"Obwód: {a * 4}");
+            Console.ReadKey();
+        }
+    }
+}
