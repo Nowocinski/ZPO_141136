b9537a65884be669e3cb8ed21ec5bc6427248933
Rafał Nowociński <vito24222@gmail.com>
2020-03-08 21:10:09 +0100

    Dodanie zadania A29

---
+++ Ćwiczenia 1/A29/A29/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace A29
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Podaj długość boku sześcianu: ");
+            double a;
+            while (!double.TryParse(Console.ReadLine(), out a) || a <= 0)
+            {
+                if (a <= 0)
+                {
+                    Console.Write("Liczba musi być większa od zera. Podaj poprawną liczbę: ");
+                    continue;
+                }
+
+                Console.Write("Podana wartość nie jest liczbą. Podaj liczbę: ");
+            }
+
+            Console.WriteLine($"Pole: {a * a * a}");
+            Console.WriteLine($"Obwód: {6 * a * a}");
+            Console.ReadKey();
+        }
+    }
+}
