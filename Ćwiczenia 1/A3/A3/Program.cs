f7fda7e97ef078beed55f5e10dde57fdeb5a8820
Rafał Nowociński <vito24222@gmail.com>
2020-03-08 16:59:31 +0100

    Dodanie zadania A3

--- Ćwiczenia 1/A3/A3/Program.cs
+++ Ćwiczenia 1/A3/A3/Program.cs
@@ -1,15 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace A3
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            Console.Write("Podaj pierwszą liczbę: ");
+            double liczba1;
+            while (!double.TryParse(Console.ReadLine(), out liczba1))
+            {
+                Console.Write("Podana wartość nie jest liczbą. Podaj liczbę: ");
+            }
+
+            Console.Write("Podaj drugą liczbę: ");
+            double liczba2;
+            while (!double.TryParse(Console.ReadLine(), out liczba2))
+            {
+                Console.Write("Podana wartość nie jest liczbą. Podaj liczbę: ");
+            }
+
+            if (liczba2 == 0)
+            {
+                Console.WriteLine($"Operacja niewykonywalna. Nie można dzielić przez zero.");
+            }
+            else
+            {
+                Console.WriteLine($"Wynik dzielenia {liczba1} przez {liczba2} to {liczba1 / liczba2}");
+            }
+
+            Console.ReadKey();
         }
     }
 }
