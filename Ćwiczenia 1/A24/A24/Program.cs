39584d256bffef51e49e953775473084f5c6636e
Rafał Nowociński <vito24222@gmail.com>
2020-03-08 20:37:09 +0100

    Dodanie zadania A24

---
+++ Ćwiczenia 1/A24/A24/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace A24
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Podaj wielkość kontową: ");
+            double liczba;
+            while (!double.TryParse(Console.ReadLine(), out liczba))
+            {
+                Console.Write("Podana wartość nie jest liczbą. Podaj liczbę: ");
+            }
+
+            Console.WriteLine($"Wartość w radianach: {(Math.PI / 180) * liczba}");
+            Console.ReadKey();
+        }
+    }
+}
