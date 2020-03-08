4908720fcc70fed4bd5770bccafc78b28c693a87
Rafał Nowociński <vito24222@gmail.com>
2020-03-08 18:29:08 +0100

    Dodanie zadania A13

---
+++ Ćwiczenia 1/A13/A13/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace A13
+{
+    class Program
+    {
+        static void Main()
+        {
+            for (var i = 1; i <= 99; i+=2)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
