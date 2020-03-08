55519d87bd287a01b7b1923d32f2ddbed99750c3
Rafał Nowociński <vito24222@gmail.com>
2020-03-08 20:08:22 +0100

    Dodanie zadania A21

---
+++ Ćwiczenia 1/A21/A21/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace A21
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Podaj wiek: ");
+            int wiek;
+            while (!int.TryParse(Console.ReadLine(), out wiek))
+            {
+                Console.Write("Podana wartość nie jest liczbą. Podaj liczbę: ");
+            }
+
+            Console.WriteLine($"Wiek: {wiek}");
+            Console.ReadKey();
+        }
+    }
+}
