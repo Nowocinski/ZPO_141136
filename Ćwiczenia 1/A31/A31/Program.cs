20097260f1042ad2609b2a676eaf702c8f727f9c
Rafał Nowociński <vito24222@gmail.com>
2020-03-08 21:18:47 +0100

    Dodanie zadania A30

---
+++ Ćwiczenia 1/A31/A31/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace A31
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Podaj długość boku A: ");
+            double a;
+            while (!double.TryParse(Console.ReadLine(), out a) || a <= 0)
+            {
+                if (a <= 0)
+                {
+                    Console.Write("Liczba musi być większa od zera. Podaj poprawną liczbę: ");
+                    continue;
+                }
+
+                Console.Write("Podana wartość nie jest liczbą. Podaj liczbę: ");
+            }
+
+            Console.Write("Podaj długość boku B: ");
+            double b;
+            while (!double.TryParse(Console.ReadLine(), out b) || b <= 0)
+            {
+                if (b <= 0)
+                {
+                    Console.Write("Liczba musi być większa od zera. Podaj poprawną liczbę: ");
+                    continue;
+                }
+
+                Console.Write("Podana wartość nie jest liczbą. Podaj liczbę: ");
+            }
+
+            Console.Write("Podaj długość boku B: ");
+            double c;
+            while (!double.TryParse(Console.ReadLine(), out c) || c <= 0)
+            {
+                if (c <= 0)
+                {
+                    Console.Write("Liczba musi być większa od zera. Podaj poprawną liczbę: ");
+                    continue;
+                }
+
+                Console.Write("Podana wartość nie jest liczbą. Podaj liczbę: ");
+            }
+
+            Console.WriteLine($"Pole: {2*a*b + 2*b*c + 2*a*c}");
+            Console.WriteLine($"Objętość: {a * b * c}");
+            Console.ReadKey();
+        }
+    }
+}
