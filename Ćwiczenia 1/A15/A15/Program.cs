7b7ef1d2a733b2baf47d431aca2c79205d88c357
Rafał Nowociński <vito24222@gmail.com>
2020-03-08 18:56:45 +0100

    Dodanie zadania A15

---
+++ Ćwiczenia 1/A15/A15/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace A15
+{
+    class Program
+    {
+        static void Main()
+        {
+            var suma = 0;
+
+            Console.Write("Podaj liczbę: ");
+            int liczba;
+            
+            while (!int.TryParse(Console.ReadLine(), out liczba))
+            {
+                Console.Write("Podana wartość nie jest liczbą. Podaj liczbę: ");
+            }
+
+            int licznaKopia = liczba;
+
+            while (true)
+            {
+                suma += liczba%10;
+
+                if (liczba < 10)
+                {
+                    break;
+                }
+
+                liczba /= 10;
+            }
+
+            Console.WriteLine($"Suma cyfr liczby {licznaKopia} to {suma}");
+            Console.ReadKey();
+        }
+    }
+}
