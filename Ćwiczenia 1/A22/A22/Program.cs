6887076ac91830f41c3bdf46900354634a792eb3
Rafał Nowociński <vito24222@gmail.com>
2020-03-08 20:21:16 +0100

    Dodanie zadania A22

---
+++ Ćwiczenia 1/A22/A22/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace A22
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("        *");
+            Console.WriteLine("       ***");
+            Console.WriteLine("      *****");
+            Console.WriteLine("*****************");
+            Console.WriteLine("  *************");
+            Console.WriteLine("    *********");
+            Console.WriteLine("   ****   ****");
+            Console.WriteLine("  **         **");
+            Console.ReadKey();
+        }
+    }
+}
