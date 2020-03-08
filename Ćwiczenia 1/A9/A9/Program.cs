3926f9072ed2f114d83b48a24c84201e16823481
Rafał Nowociński <vito24222@gmail.com>
2020-03-08 17:53:47 +0100

    Dodanie zadania A9

---
+++ Ćwiczenia 1/A9/A9/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace A9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Wprowadź tekst: ");
+            var tekst = Console.ReadLine();
+
+            if (tekst.Length < 2)
+            {
+                Console.WriteLine("Tekst jest za krótki do sprócenia");
+            }
+            else
+            {
+                Console.WriteLine(tekst.Remove(1, 1));
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
