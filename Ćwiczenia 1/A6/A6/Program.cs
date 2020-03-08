b43f503821b77222c81e6bd518d3aa11c839d222
Rafał Nowociński <vito24222@gmail.com>
2020-03-08 17:28:19 +0100

    Dodanie zadania A6

---
+++ Ćwiczenia 1/A6/A6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace A6
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine($"{'x', 5}{0, 5}{1, 5}{2, 5}{3, 5}{4, 5}{5, 5}");
+            for (var i = 0; i <= 5; i++)
+            {
+                Console.Write($"{i, 5}");
+
+                for (var j = 0; j <= 5; j++)
+                {
+                    Console.Write($"{i*j, 5}");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
