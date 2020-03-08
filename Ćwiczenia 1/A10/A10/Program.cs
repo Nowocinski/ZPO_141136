737fa9139f6be8d579f1bd9c0c338a25c9501308
Rafał Nowociński <vito24222@gmail.com>
2020-03-08 17:59:49 +0100

    Dodanie zadania A10

---
+++ Ćwiczenia 1/A10/A10/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace A10
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Wprowadź tekst: ");
+            var tekst = Console.ReadLine();
+
+            if (tekst.Length < 2)
+            {
+                Console.WriteLine("Tekst jest za krótki");
+            }
+            else
+            {
+                var pierwszyZnak = tekst.First();
+                var ostatniZnak = tekst.Last();
+                tekst = tekst.Remove(0, 1);
+                tekst = tekst.Remove(tekst.Length - 1, 1);
+                Console.WriteLine($"{ostatniZnak}{tekst}{pierwszyZnak}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
