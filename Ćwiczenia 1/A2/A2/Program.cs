8537b6e0bf606c62fba3ee2b78d60a99f323915a
Rafał Nowociński <vito24222@gmail.com>
2020-03-08 16:51:32 +0100

    Dodanie zadania A2

---
+++ Ćwiczenia 1/A2/A2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace A2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Podaj pierwszą liczbę: ");
+            double liczba1;
+            while (!double.TryParse(Console.ReadLine(), out liczba1))
+            {
+                Console.Write("Podana wartość nie jest liczbą. Podaj liczbę: ");
+            }
+
+            Console.Write("Podaj drugą liczbę: ");
+            double liczba2;
+            while (!double.TryParse(Console.ReadLine(), out liczba2))
+            {
+                Console.Write("Podana wartość nie jest liczbą. Podaj liczbę: ");
+            }
+
+            Console.WriteLine($"Suma liczb {liczba1} i {liczba2} to {liczba1+liczba2}");
+            Console.ReadKey();
+        }
+    }
+}
