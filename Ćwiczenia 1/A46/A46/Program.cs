a9b60405907172da4fd22b470eb154007cb8ff86
Rafał Nowociński <vito24222@gmail.com>
2020-03-09 16:32:09 +0100

    Dodanie zdania A46

---
+++ Ćwiczenia 1/A46/A46/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace A46
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Podaj liczbę: ");
+            int liczba;
+            while (!int.TryParse(Console.ReadLine(), out liczba))
+            {
+                Console.Write("Podana wartość nie jest liczbą całkowitą. Podaj liczbę: ");
+            }
+
+            if (liczba <= 12 && liczba > 0)
+            {
+                Console.WriteLine($"Liczba {liczba} może być numerem miesiąca");
+            }
+            else
+            {
+                Console.WriteLine($"Liczba {liczba} nie może być numerem miesiąca");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
