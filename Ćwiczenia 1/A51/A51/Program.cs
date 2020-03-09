f925097a7b72ccf8e77538dd9cbfc18031c3b957
Rafał Nowociński <vito24222@gmail.com>
2020-03-09 16:57:21 +0100

    Dodanie zadania A54

---
+++ Ćwiczenia 1/A51/A51/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace A51
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Podaj liczbę: ");
+            int liczba;
+            while (!int.TryParse(Console.ReadLine(), out liczba))
+            {
+                Console.Write("Podana wartość nie jest liczbą całkowitą. Podaj liczbę: ");
+            }
+
+            if (liczba >= 100 & liczba < 1000)
+            {
+                Console.WriteLine("Liczba musi składać się z 3 cyfr.");
+            }
+
+            if (liczba%10 == liczba/100)
+            {
+                Console.WriteLine($"Liczba {liczba} jest palindromem.");
+            }
+            else
+            {
+                Console.WriteLine($"Liczba {liczba} nie jest palindromem.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
