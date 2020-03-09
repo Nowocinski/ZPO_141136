2dd0d951b22efc19177f62fe9072f50f6637a65c
Rafał Nowociński <vito24222@gmail.com>
2020-03-09 17:06:15 +0100

    Dodanie zadania A54

---
+++ Ćwiczenia 1/A54/A54/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace A54
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Podaj liczbę: ");
+            int liczba;
+            while (!int.TryParse(Console.ReadLine(), out liczba))
+            {
+                Console.Write("Podana wartość nie jest liczbą całkowitą. Podaj liczbę: ");
+            }
+
+            var i = 0;
+            while (i <= liczba)
+            {
+                for (var j = 1; j <= i; j++)
+                {
+                    Console.Write($"{j, 2}");
+                }
+                i++;
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
