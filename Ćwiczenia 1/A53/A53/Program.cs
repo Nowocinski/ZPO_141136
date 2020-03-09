0f5ce70629142ac87eb84f3bfd3c799afb59e5e1
Rafał Nowociński <vito24222@gmail.com>
2020-03-09 17:01:37 +0100

    Dodanie zadania A53

---
+++ Ćwiczenia 1/A53/A53/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace A53
+{
+    class Program
+    {
+        static void Main()
+        {
+            for (var i = 0; i < 3; i++)
+            {
+                for (var j = 0; j < 20; j++)
+                {
+                    Console.Write("P");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
