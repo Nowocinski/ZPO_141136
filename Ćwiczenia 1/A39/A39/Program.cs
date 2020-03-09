a9b63f86402b9df262244beeb536c3b50c9e15a9
Rafał Nowociński <vito24222@gmail.com>
2020-03-09 15:56:30 +0100

    Dodanie zadania A39

---
+++ Ćwiczenia 1/A39/A39/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace A39
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Podaj liczbę: ");
+            double liczba;
+            while (!double.TryParse(Console.ReadLine(), out liczba))
+            {
+                Console.Write("Podana wartość nie jest liczbą. Podaj liczbę: ");
+            }
+
+            if (liczba < 0)
+            {
+                Console.WriteLine("Nie można pierwiastkować liczb ujemnych.");
+            }
+            else
+            {
+                var wynik = Math.Sqrt(liczba);
+                Console.WriteLine($"Wynik: {wynik}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
