b15bec71f9a1f73cf7dc450e24d0aef01844213a
Rafał Nowociński <vito24222@gmail.com>
2020-03-09 16:35:55 +0100

    Dodanie zadania A47

---
+++ Ćwiczenia 1/A47/A47/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace A47
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Podaj liczbę: ");
+            int liczba;
+            while (!int.TryParse(Console.ReadLine(), out liczba))
+            {
+                Console.Write("Podana wartość nie jest liczbą całkowitą. Podaj liczbę: ");
+            }
+
+            if (liczba % 4 == 0 && liczba % 100 != 0 || liczba % 400 == 0)
+            {
+                Console.WriteLine("Podany liczba może być rokiem przestepny.");
+            }
+            else
+            {
+                Console.WriteLine("Podany liczba nie może być rokiem przestepny.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
