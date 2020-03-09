f0003b97c5ff2304d913af92c88a7d3b52f16d9f
Rafał Nowociński <vito24222@gmail.com>
2020-03-09 15:47:54 +0100

    Dodanie zadania A37

---
+++ Ćwiczenia 1/A37/A37/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace A37
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine((DateTime.Now - new DateTime(DateTime.Now.Year, 1, 1)).TotalHours);
+            Console.ReadKey();
+        }
+    }
+}
