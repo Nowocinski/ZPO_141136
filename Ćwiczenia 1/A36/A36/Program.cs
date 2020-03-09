1dabeaaf140d717d5bde7c1c3fb2a2d5693d324e
Rafał Nowociński <vito24222@gmail.com>
2020-03-09 15:45:39 +0100

    Dodanie zadania A36

---
+++ Ćwiczenia 1/A36/A36/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace A36
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine((DateTime.Now - new DateTime(DateTime.Now.Year, 1, 1)).TotalDays);
+            Console.ReadKey();
+        }
+    }
+}
