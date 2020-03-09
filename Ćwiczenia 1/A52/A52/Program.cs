219073e5eabdb5dae46a5aeefcfdca524923403c
Rafał Nowociński <vito24222@gmail.com>
2020-03-09 16:59:41 +0100

    Dodanie zadania A52

---
+++ Ćwiczenia 1/A52/A52/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace A52
+{
+    class Program
+    {
+        static void Main()
+        {
+            for (var i = 0; i < 17; i++)
+            {
+                Console.Write("*");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
