547d53afcf2355afdb9b346d953c38e3a317960a
Rafał Nowociński <vito24222@gmail.com>
2020-03-09 16:30:20 +0100

    Dodanie zadania 45

---
+++ Ćwiczenia 1/A45/A45/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace A45
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Podaj liczbę: ");
+            int liczba;
+            while (!int.TryParse(Console.ReadLine(), out liczba))
+            {
+                Console.Write("Podana wartość nie jest liczbą całkowitą. Podaj liczbę: ");
+            }
+
+            if (liczba <= 31 && liczba > 0)
+            {
+                Console.WriteLine($"Liczba {liczba} może być dniem miesiąca");
+            }
+            else
+            {
+                Console.WriteLine($"Liczba {liczba} nie może być dniem miesiąca");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
