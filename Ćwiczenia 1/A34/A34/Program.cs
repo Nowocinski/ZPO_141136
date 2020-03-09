bdf58a016e54efaf11c3ed44695b31e19c59ce93
Rafał Nowociński <vito24222@gmail.com>
2020-03-09 15:42:09 +0100

    Dodanie zadania A34

---
+++ Ćwiczenia 1/A34/A34/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace A34
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine((DateTime.Now - new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day)).TotalSeconds);
+            Console.ReadKey();
+        }
+    }
+}
