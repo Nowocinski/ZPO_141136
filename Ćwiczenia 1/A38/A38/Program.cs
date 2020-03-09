d350857d9baf74dd47195d56cb7d1cc50b5fa744
Rafał Nowociński <vito24222@gmail.com>
2020-03-09 15:52:42 +0100

    Dodanie zadania A38

---
+++ Ćwiczenia 1/A38/A38/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace A38
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Podaj pierwszą liczbę: ");
+            int liczba1;
+            while (!int.TryParse(Console.ReadLine(), out liczba1))
+            {
+                Console.Write("Podana wartość nie jest liczbą. Podaj liczbę: ");
+            }
+
+            Console.Write("Podaj drugą liczbę: ");
+            int liczba2;
+            while (!int.TryParse(Console.ReadLine(), out liczba2))
+            {
+                Console.Write("Podana wartość nie jest liczbą. Podaj liczbę: ");
+            }
+
+            try
+            {
+                var wynik = liczba1 / liczba2;
+                Console.WriteLine($"Wynik: {wynik}");
+            }
+            catch
+            {
+                Console.WriteLine("Nie można dzielić przez zero.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
