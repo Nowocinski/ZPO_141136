c2cc4aa36cbb7a322f1d6e60fdb70e6d83aba5f8
Rafał Nowociński <vito24222@gmail.com>
2020-03-09 16:45:18 +0100

    Dodanie zadania 49

---
+++ Ćwiczenia 1/A49/A49/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace A49
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Podaj liczbę (godziny): ");
+            int godzina;
+            while (!int.TryParse(Console.ReadLine(), out godzina))
+            {
+                Console.Write("Podana wartość nie jest liczbą całkowitą. Podaj liczbę: ");
+            }
+
+            Console.Write("Podaj liczbę (minuty): ");
+            int minuta;
+            while (!int.TryParse(Console.ReadLine(), out minuta))
+            {
+                Console.Write("Podana wartość nie jest liczbą całkowitą. Podaj liczbę: ");
+            }
+
+            Console.Write("Podaj liczbę (sekundy): ");
+            int sekunda;
+            while (!int.TryParse(Console.ReadLine(), out sekunda))
+            {
+                Console.Write("Podana wartość nie jest liczbą całkowitą. Podaj liczbę: ");
+            }
+
+            try
+            {
+                var data = new DateTime(2000, 1, 1, godzina, minuta, sekunda);
+                Console.WriteLine($"Można utworzyć czasu {godzina}:{minuta}:{sekunda}.");
+            }
+            catch
+            {
+                Console.WriteLine($"Nie można utworzyć czasu {godzina}:{minuta}:{sekunda}.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
