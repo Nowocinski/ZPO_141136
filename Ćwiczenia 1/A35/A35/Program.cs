eb7b2671d06b87078e26a2e778b51bdcad98dda9
Rafał Nowociński <vito24222@gmail.com>
2020-03-09 15:44:09 +0100

    Dodanie zadania A35

---
+++ Ćwiczenia 1/A35/A35/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace A35
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine((DateTime.Now - new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day)).TotalMinutes);
+            Console.ReadKey();
+        }
+    }
+}
