a2f3941bc4587ac59db8d7105b260a63c88a863e
Rafał Nowociński <vito24222@gmail.com>
2020-03-09 15:58:51 +0100

    Dodanie zadania A40

---
+++ Ćwiczenia 1/A40/A40/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace A40
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Podaj liczbę: ");
+            int liczba;
+            while (!int.TryParse(Console.ReadLine(), out liczba))
+            {
+                Console.Write("Podana wartość nie jest liczbą. Podaj liczbę: ");
+            }
+
+            if (liczba % 2 == 0)
+            {
+                Console.WriteLine("Liczba jest parzysta.");
+            }
+            else
+            {
+                Console.WriteLine("Liczba nie jest parzysta.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
