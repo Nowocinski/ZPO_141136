c89ce9e54c0359667d0ab5e7d76a817a8317a0e2
Rafał Nowociński <vito24222@gmail.com>
2020-03-09 16:41:35 +0100

    Dodanie zadania A48

---
+++ Ćwiczenia 1/A48/A48/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace A48
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Podaj liczbę (dni): ");
+            int dni;
+            while (!int.TryParse(Console.ReadLine(), out dni))
+            {
+                Console.Write("Podana wartość nie jest liczbą całkowitą. Podaj liczbę: ");
+            }
+
+            Console.Write("Podaj liczbę (miesiąc): ");
+            int miesiąc;
+            while (!int.TryParse(Console.ReadLine(), out miesiąc))
+            {
+                Console.Write("Podana wartość nie jest liczbą całkowitą. Podaj liczbę: ");
+            }
+
+            Console.Write("Podaj liczbę (rok): ");
+            int rok;
+            while (!int.TryParse(Console.ReadLine(), out rok))
+            {
+                Console.Write("Podana wartość nie jest liczbą całkowitą. Podaj liczbę: ");
+            }
+
+            try
+            {
+                var data = new DateTime(rok, miesiąc, dni);
+                Console.WriteLine($"Można utworzyć daty {dni}-{miesiąc}-{rok}.");
+            }
+            catch
+            {
+                Console.WriteLine($"Nie można utworzyć datę {dni}-{miesiąc}-{rok}.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
