413c693ceb5c92b66b8d763b0c9de863ddf1d81b
Rafał Nowociński <vito24222@gmail.com>
2020-03-20 16:04:36 +0100

    Dodanie zadania B3

---
+++ Ćwiczenia 2/B3/B3/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace B3
+{
+    class Program
+    {
+        static void Main()
+        {
+            Wycieczka w1 = new Wycieczka();
+            Wycieczka w2 = new Wycieczka();
+            Wycieczka w3 = new Wycieczka();
+
+            w1.ObliczCene();
+            w1.kraj = "Polska";
+
+            w2.ObliczCene();
+            w2.hotelGwiazdki = 5;
+
+            // Wykonanie metody z przypisaniem wartości do pola wewnątrz metody
+            w3.UstawallInclusive(true);
+        }
+    }
+}
---
+++ Ćwiczenia 2/B3/B3/Wycieczka.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace B3
+{
+    class Wycieczka
+    {
+        private int cena;
+        public string kraj;
+        private bool allInclusive;
+        public int hotelGwiazdki;
+
+        public void ObliczCene()
+        {
+
+        }
+
+        protected void WyswietlCene()
+        {
+            Console.WriteLine($"Cena: {this.cena}");
+        }
+
+        public void UstawallInclusive(bool allInclusive)
+        {
+            this.allInclusive = allInclusive;
+        }
+    }
+}
