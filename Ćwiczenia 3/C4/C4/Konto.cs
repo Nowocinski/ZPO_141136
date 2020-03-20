2cfa7beb5bbaf5deec259ff7c23f7119c6b557fe
Rafał Nowociński <vito24222@gmail.com>
2020-03-20 17:55:48 +0100

    Dodanie zadania C4

---
+++ Ćwiczenia 3/C4/C4/Konto.cs
@@ -0,0 +1,53 @@
+﻿namespace C4
+{
+    class Konto
+    {
+        public int saldoPoczątkowe;
+        public int saldoKońcowe;
+        public int przelewMiedzyKontami;
+        public int przelewZewnętrzny;
+        public int wplata;
+        public int wyplat;
+
+        public Konto()
+        {
+            
+        }
+
+        public Konto(int saldoPoczątkowe, int saldoKońcowe, int przelewMiedzyKontami, int przelewZewnętrzny, int wplata, int wyplat)
+        {
+            this.saldoPoczątkowe = saldoPoczątkowe;
+            this.saldoKońcowe = saldoKońcowe;
+            this.przelewMiedzyKontami = przelewMiedzyKontami;
+            this.przelewZewnętrzny = przelewZewnętrzny;
+            this.wplata = wplata;
+            this.wyplat = wyplat;
+        }
+
+        public int RóżnicaWpłątIWypłat()
+        {
+            int różnica = wplata - wyplat;
+            if (różnica > 0)
+            {
+                return różnica;
+            }
+
+            return -różnica;
+        }
+
+        public void ZmieńSaldoPoczątkowe(int saldoPoczątkowe)
+        {
+            this.saldoPoczątkowe = saldoPoczątkowe;
+        }
+
+        public void WczyczyśćWszystkieDane()
+        {
+            this.saldoPoczątkowe = 0;
+            this.saldoKońcowe = 0;
+            this.przelewMiedzyKontami = 0;
+            this.przelewZewnętrzny = 0;
+            this.wplata = 0;
+            this.wyplat = 0;
+        }
+    }
+}
---
+++ Ćwiczenia 3/C4/C4/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace C4
+{
+    class Program
+    {
+        static void Main()
+        {
+            Konto konto1 = new Konto();
+            Konto konto2 = new Konto();
+            Konto konto3 = new Konto();
+
+            konto1.RóżnicaWpłątIWypłat();
+            konto2.ZmieńSaldoPoczątkowe(20);
+            konto3.WczyczyśćWszystkieDane();
+        }
+    }
+}
