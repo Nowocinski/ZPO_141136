6d9875f49a9c6b03281acb932baa322f92c5fbc8
Rafał Nowociński <vito24222@gmail.com>
2020-03-20 17:43:55 +0100

    Dodanie zadania C3

---
+++ Ćwiczenia 3/C3/C3/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace C3
+{
+    class Program
+    {
+        static void Main()
+        {
+            OpiszTyp();
+            OpiszTyp(2);
+            OpiszTyp("Test");
+            OpiszTyp(2, 5);
+        }
+
+        static string OpiszTyp()
+        {
+            return "Metoda bez argumentów";
+        }
+
+        static string OpiszTyp(int x)
+        {
+            return "Liczba całkowita";
+        }
+
+        static string OpiszTyp(string x)
+        {
+            return "Łańcuch znaków";
+        }
+
+        static string OpiszTyp(int x, int y)
+        {
+            return "Dwie liczby calkowite";
+        }
+    }
+}
