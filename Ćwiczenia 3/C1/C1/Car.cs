4885a88539325063549aa35c725c023e668e2dfe
Rafał Nowociński <vito24222@gmail.com>
2020-03-20 17:32:34 +0100

    Dodanie zadania C1

---
+++ Ćwiczenia 3/C1/C1/Car.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace C1
+{
+    class Car
+    {
+        private double pojemonoscSilnika;
+        private string marka;
+        public static int iloscKol;
+
+        public Car()
+        {
+            
+        }
+
+        public Car(double pojemonoscSilnika, string marka)
+        {
+            this.pojemonoscSilnika = pojemonoscSilnika;
+            this.marka = marka;
+        }
+
+        public static Car Create()
+        {
+            return new Car();
+        }
+
+        static Car()
+        {
+            iloscKol = 4;
+        }
+
+        ~Car()
+        {
+            Console.WriteLine("Zwalniam pamięć");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Ćwiczenia 3/C1/C1/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace C1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Car car = new Car();
+
+            // Stwórz obiekt typuCar, czy masz dostęp do jego pól? czy możesz zmienić wartości? (nie zmieniaj pólklasy na publiczne).
+            // Odpowiedź: Nie ma dostępu do pól obiektu Car. Nie można przypisać wartości do pól.
+            // car.pojemonoscSilnika;
+            // car.marka;
+
+            Car car2 = new Car(125d, "Fiat");
+
+            // Zmień modyfikatory dostępu konstruktorów na private.
+            // Czy jest możliwe utworzenie nowego obiektutej klasy? Odpowiedź: Nie jest możliwe tworzenie obiektu dla prywatnych konstruktorów
+            // Co się stało z konstruktorem domyślnym? Odpowiedź: Konstruktor domyślny zostanie przesłonięty konstruktorami prywatnymi
+
+            // Następnie stwórz statyczny konstruktor ustawiający wartość tego pola na 4.
+            // Jak możesz odwołać się do pola? Odpowiedź: Car.iloscKol o ile iloscKol nie jest polem prywatnym
+            // Czy możesz go zmodyfikować? Odpowiedź: np. Car.iloscKol = 10
+            // Co się stanie jak dodasz readonly? Odpowiedź: Przypisanie wartości do ilosc Kol jest możliwe tylko w konstruktorze statycznym
+
+            Car.iloscKol = 2;
+            Car.iloscKol = 4;
+
+            // Dodaj w klasieCarstałe pole iloscDrzwi i ustaw mu wartość 4.
+            // Jak możesz się do niego odnieść? Odpowiedź: Car.iloscDrzwi
+            // Czymożesz go zmodyfikować? Odpowiedź: Nie
+        }
+    }
+}
