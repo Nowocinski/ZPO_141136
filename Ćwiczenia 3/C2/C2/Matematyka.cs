317b80936dd88781b2fc1aa1a50344e3ba8728d9
Rafał Nowociński <vito24222@gmail.com>
2020-03-20 17:39:48 +0100

    Dodanie zadania C2

---
+++ Ćwiczenia 3/C2/C2/Matematyka.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace C2
+{
+    class Matematyka
+    {
+        public static double pi = 3.14;
+
+        public static double ObliczPoleKoła(double r)
+        {
+            if (r < 0)
+            {
+                throw new Exception("Promień koła nie może mieć wartości ujemnej.");
+            }
+
+            return pi * r * r;
+        }
+
+        public static double ObliczObwódKoła(double r)
+        {
+            if (r < 0)
+            {
+                throw new Exception("Promień koła nie może mieć wartości ujemnej.");
+            }
+
+            return 2 * pi * r;
+        }
+    }
+}
---
+++ Ćwiczenia 3/C2/C2/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace C2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Matematyka.ObliczObwódKoła(20);
+            Matematyka.ObliczPoleKoła(2);
+        }
+    }
+}
